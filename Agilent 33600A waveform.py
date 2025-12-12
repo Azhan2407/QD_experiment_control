cbfe75eb27110ae79bc0e5a7896bf7c5202422c8
dt360-sussex <dt360@sussex.ac.uk>
2025-12-12 16:58:51 +0000

    Initial commit

---
+++ Agilent 33600A waveform.py
@@ -0,0 +1,27 @@
+# import pyvisa
+# rm = pyvisa.ResourceManager('@py')
+# rm.list_resources()
+# inst = rm.open_resource(ip)
+# print(inst.query("*IDN?"))
+
+
+from pylablib.devices import AWG
+
+
+ip = 'TCPIP::169.254.11.23::INSTR'
+
+
+
+class Agilent33600A(AWG.GenericAWG):
+    """Minimal driver wrapper for Keysight/Agilent 33600A series AWGs."""
+    def __init__(self, addr):
+        self._channels_number = 2  # 33600A has 2 channels
+        super().__init__(addr)
+
+
+awg=Agilent33600A(ip)
+
+print(awg.set_function("arb", 2))
+
+awg.write()
+awg.close()
